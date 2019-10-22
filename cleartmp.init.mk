6e85cffb36fc2d433e639111e9b84d898e97133d
arcade <>
2019-10-22 08:34:05 +0000

    fix scripts
    FossilOrigin-Name: 29803f148a80807e258a2344a8a1d9f5084d60e7d5d0712fbeb313f3566fe7ea

--- cleartmp.init.mk
+++ cleartmp.init.mk
@@ -3,7 +3,7 @@ cleartmp: mountlate
 	find -x /tmp -mindepth 1 ! -name lost+found \
 	    ! -name snapshots ! -path "./snapshots/*" \
 	    ! -name quota.user ! -name quota.group \
-	    -exec rm -rf -- {} \; -type d -prune); \
-	  rm -f /tmp/.X*-lock; \
-	  rm -fr /tmp/.X11-unix; \
+	    -delete -type d -prune ;\
+	  rm -f /tmp/.X*-lock ;\
+	  rm -fr /tmp/.X11-unix ;\
 	  mkdir -m 1777 /tmp/.X11-unix
--- fsck.init.mk
+++ fsck.init.mk
@@ -1,35 +1,32 @@
 fsck:
 	echo "MRC:$@> Checking disks." ;\
 	fsck -p ;\
-	case $$? in ;\
-	0) ;\
-	  ;; ;\
-	2) ;\
-	  exit 1;\
-	  ;; ;\
-	4) ;\
-	  echo "Rebooting..." ;\
-	  reboot ;\
-	  echo "Reboot failed; help!" ;\
-	  exit 1 ;\
-	  ;; ;\
-	8) ;\
-	  if [ -n "$${FSCK_Y_ENABLE}" ]; then \
-	    echo "File system preen failed, trying fsck -y." ;\
-	    fsck -y || {\
-	      echo "Automatic file system check failed; help!" ;\
-	      exit 1 '\
-	    }\
-	  else \
-	    echo "Automatic file system check failed; help!" ;\
-	    exit 1 ;\
-	  fi ;\
-	12) ;\
-	  echo "Boot interrupted." ;\
-	130) ;\
-	  exit 1 ;\
-	*) ;\
-	  echo "Unknown error, help!" ;\
-	  exit 1 ;\
-	  ;; ;\
+	case $$? in \
+	0) ;; \
+	2) exit 1 \
+	   ;; \
+	4) echo "Rebooting..." ;\
+	   reboot ;\
+	   echo "Reboot failed; help!" ;\
+	   exit 1 \
+	   ;; \
+	8) if [ -n "$${FSCK_Y_ENABLE}" ]; then \
+	     echo "File system preen failed, trying fsck -y." ;\
+	     fsck -y || {\
+	       echo "Automatic file system check failed; help!" ;\
+	       exit 1 ;\
+	     }\
+	   else \
+	     echo "Automatic file system check failed; help!" ;\
+	     exit 1 ;\
+	   fi ;\
+	   ;; \
+	12) echo "Boot interrupted." ;\
+	   exit 1 \
+	   ;; \
+	130) exit 1 \
+	   ;; \
+	*) echo "Unknown error, help!" ;\
+	   exit 1 \
+	   ;; \
 	esac
