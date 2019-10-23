7387613c83aff642d0f215fc5da3dae6199c48c5
arcade <>
2019-10-23 07:40:41 +0000

    make interface configuration multiline
    FossilOrigin-Name: c9cd53b0f66586015aeac4c088ee2982d06665dcfa8756b638f1ba54d4ea75b1

--- ifconfig.init.mk
+++ ifconfig.init.mk
@@ -1,8 +1,10 @@
 IFCONFIG_IFACES?=lo0
-IFCONFIG_lo0?=inet 127.0.0.1/8 up
+IFCONFIG_lo0?="inet 127.0.0.1/8 up"
 
 ifconfig: adjkerntz wlans cloned kld
 	echo "MRC:$@> Starting interfaces: ${IFCONFIG_IFACES}"
 .for iface in ${IFCONFIG_IFACES}
-	ifconfig ${iface} ${IFCONFIG_${iface}}
+.for item in ${IFCONFIG_${iface}}
+	ifconfig ${iface} ${item}
+.endfor
 .endfor
