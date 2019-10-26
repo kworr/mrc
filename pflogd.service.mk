e9660f027e04bd36d7fb1636500dd4895cdab048
arcade <>
2019-10-26 07:08:04 +0000

    fix bugs in pflog
    FossilOrigin-Name: 475dddeba731af91c3d863fd53ac622d2d932467f23b5b7e82e9cd5e1c340255

--- pflogd.service.mk
+++ pflogd.service.mk
@@ -2,12 +2,12 @@ DAEMON_pflogd_COMMAND?=/sbin/pflogd
 DAEMON_pflogd_MODULES?=pf
 DAEMON_pflogd_FLAGS?=-f /var/log/pflog
 
-pflogd: roor mount netif
+pflogd: root mount netif
 .if !empty(PFLOG_ENABLE:tl:Mno)
 	echo "MRC:$@> Configuring device." ;\
 	kldload -n pf || exit 1 ;\
 	ifconfig pflog0 up || { \
 	  echo "MRC:$@> Failed to set up pflog0 device." ;\
 	  exit 1 ;\
-	} \
+	}
 .endif
