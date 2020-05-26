1be2ded4853b8c562c62352785702044882211c9
arcade <>
2020-05-26 07:02:15 +0000

    clean up
    FossilOrigin-Name: 8f163fdf5ea2fb7a0e3b8c5984ab58727927515790713ecfebbc8febb113f87c

--- boinc-client.service.mk
+++ boinc-client.service.mk
@@ -2,5 +2,3 @@ DAEMON_boinc-client_COMMAND?=/usr/local/bin/boinc_client
 DAEMON_boinc-client_FLAGS?=--redirectio   --dir /var/db/boinc
 DAEMON_boinc-client_BACKGROUND?=-daemon
 DAEMON_boinc-client_USER?=boinc
-
-boinc-client: _service
--- powerd.service.mk
+++ powerd.service.mk
@@ -1,4 +1,2 @@
 DAEMON_powerd_COMMAND?=/usr/sbin/powerd
 DAEMON_powerd_ENABLE?=no
-
-powerd: _service # DAEMON -> _service
