01911934fe100743d7564b939c072b5025c7fff9
arcade <>
2022-05-29 11:33:21 +0000

    fix service name
    FossilOrigin-Name: 72bda3bbe4100908840d7c7b4d8cac7ec155fcc3c88f02955b92737e4a563510

--- pflogd.service.mk
+++ pflogd.service.mk
@@ -3,7 +3,7 @@ DAEMON_pflogd_MODULES?=pf
 DAEMON_pflogd_FLAGS?=-f /var/log/pflog
 
 pflogd: root mount netif
-.if !empty(PFLOG_ENABLE:tl:Mno)
+.if !empty(DAEMON_pflog_ENABLE:tl:Mno)
 	echo "MRC:$@> Configuring device." ;\
 	kldload -n pf || exit 1 ;\
 	ifconfig pflog0 up || { \
