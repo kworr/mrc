de136c54ae788aaa7384c49e4363407c39a7adf1
arcade <>
2022-08-16 12:43:09 +0000

    simplify slim
    FossilOrigin-Name: 15b16c86f22292decd927e60a4966313e72b4a40ce7ac03820364f61ad946fd0

--- slim.service.mk
+++ slim.service.mk
@@ -1,10 +1,7 @@
 DAEMON_slim_COMMAND?=/usr/local/bin/slim
-DAEMON_slim_ENABLE?=no
 DAEMON_slim_BACKGROUND?=-d
 
 DAEMON_dbus_DEPS+=slim
 
 slim: ${_SERVICE} dbus
 	rm -f /var/run/slim.auth
-
-DAEMON_EXIT: slim_exit
