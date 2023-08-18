6519d558c2197957ac3980ed324953c4b3659285
arcade <>
2023-08-18 13:38:52 +0000

    add wpa_supplicant from ports
    FossilOrigin-Name: c3e160beba2a46b17f36082c0cab164c01ae1f701b1cd68f114c949766ac16ac

--- wpa_supplicant.service.mk
+++ wpa_supplicant.service.mk
@@ -1,4 +1,8 @@
+.if exists(/usr/local/sbin/wpa_supplicant)
+DAEMON_wpa_supplicant_COMMAND?=/usr/local/sbin/wpa_supplicant
+.elif exists(/usr/sbin/wpa_supplicant)
 DAEMON_wpa_supplicant_COMMAND?=/usr/sbin/wpa_supplicant
+.endif
 DAEMON_wpa_supplicant_BACKGROUND?=-B
 DAEMON_wpa_supplicant_ENABLE?=no
 DAEMON_wpa_supplicant_FLAGS?=-M -iwlan* -Dbsd -c/etc/wpa_supplicant.conf
