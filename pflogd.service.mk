4a208702321c88d2068a9331f7353d676ecbe609
arcade <>
2023-11-04 08:49:56 +0000

    load pf module only once
    FossilOrigin-Name: 202b534ed4bc9dc5f638d5f0c10b83135674d0b41b673ab128299dfcca821061

--- pflogd.service.mk
+++ pflogd.service.mk
@@ -5,7 +5,6 @@ DAEMON_pflogd_FLAGS?=-f /var/log/pflog
 pflogd: root mount netif
 .if !empty(DAEMON_pflog_ENABLE:tl:Mno)
 	echo "MRC:$@> Configuring device." ;\
-	kldload -n pf || exit 1 ;\
 	ifconfig pflog0 up || { \
 		echo "MRC:$@> Failed to set up pflog0 device." ;\
 		exit 1 ;\
