96110127ad995d248d14add6485a17308a2e6088
arcade <>
2023-11-04 08:49:10 +0000

    fix binary selection, stop when no binary, add idprio/rtprio
    FossilOrigin-Name: adf4eb9997ae3d4ef56cb3ad16eed78519e9f7831fe00785d1d0953936447adb

--- starter.mk
+++ starter.mk
@@ -6,14 +6,25 @@ _service_check: .USEBEFORE
 	# check whether service is enabled
 	if [ -z "$${DAEMON_$@_ENABLE}" -a -z "${FORCE}" ]; then \
 		exit 0 ;\
-	fi ;\
-	for CMD in ${DAEMON_$@_COMMAND}; do \
-		if [ -x $${CMD} ]; then \
-			export CMD ;\
+	fi
+	# check for first present executable
+	for COMMAND in ${DAEMON_$@_COMMAND}; do \
+		if [ -x $${COMMAND} ]; then \
+			export CMD="${COMMAND}" ;\
 			break ;\
 		fi ;\
-	done ;\
-	echo $${CMD}
+	done
+	# bail out if binary not found
+	if [ -z $${CMD} ]; then \
+		MRC:$@> Executable not found.
+		exit 0 ;\
+	fi
+	# check for rtprio/idprio
+	if [ -n "$${DAEMON_$@_IDPRIO}" ]; then \
+		export CMD="/usr/sbin/idprio $${DAEMON_$@_IDPRIO} $${CMD}" ;\
+	elif [ -n "$${DAEMON_$@_RTPRIO}" ]; then \
+		export CMD="/usr/sbin/rtprio $${DAEMON_$@_RTPRIO} $${CMD}" ;\
+	fi
 
 _service_pre: .USEBEFORE
 	# kldload modules if any
