f780fd29a2a3717a94178e45aa15972de0196a66
arcade <>
2025-02-10 19:16:43 +0000

    fix newsyslog
    FossilOrigin-Name: b4ac8bc6180013ebd9ef821f57262c2f6aca3a94cbe48a68de30775db10489d6

--- cron.service.mk
+++ cron.service.mk
@@ -1,4 +1,6 @@
 DAEMON_cron_COMMAND?=/usr/sbin/cron
 DAEMON_cron_ENABLE?=yes
 
-cron: ${_SERVICE}
+# using daemon so that cron can start newsyslog which is sending signals
+# to other processes
+cron: ${_SERVICE_DAEMON}
