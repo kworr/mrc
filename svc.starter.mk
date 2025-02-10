21b39594fc260b6e6aec701ffa8df2e2accecc70
arcade <>
2025-02-10 19:17:04 +0000

    silence up a bit
    FossilOrigin-Name: fb383ef4d2d10c17d1294b55af6b0720a646168894f1901349c33319db05f5a7

--- svc.starter.mk
+++ svc.starter.mk
@@ -1,7 +1,7 @@
 _service_svc: .USE
 	cd ${DAEMON_$@_CWD} ;\
 	svc list $@ | grep -q $@ || \
-		svc -u ${DAEMON_$@_USER} -g ${DAEMON_$@_GROUP} ${DAEMON_$@_RESTART:D-r} ${DAEMON_$@_RESTART} init $@ $${CMD} ${DAEMON_$@_FLAGS} ${DAEMON_$@_FOREGROUND}
+		svc -u ${DAEMON_$@_USER} -g ${DAEMON_$@_GROUP} ${DAEMON_$@_RESTART:D-r} ${DAEMON_$@_RESTART} init $@ $${CMD} ${DAEMON_$@_FLAGS} ${DAEMON_$@_FOREGROUND} > /dev/null
 
 _service_svc_status: .USE
 	svc status ${@:S/_status//}
@@ -9,4 +9,4 @@ _service_svc_status: .USE
 _service_svc_exit: .USEBEFORE
 	#svc list ${@:S/_exit//} | grep -q ${@:S/_exit//} || true &&
 	lockf -kst 0 /var/run/service.${@:S/_exit//}.pid svc -t 0 kill ${@:S/_exit//} || \
-		svc -s exit ${@:S/_exit//}
+		svc -s exit ${@:S/_exit//} > /dev/null
